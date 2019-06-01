5a5992cd720c05f1d1eea556772e4956b10edb43
dmitrii.vokuev <dmitrii.vokuev@smaato.com>
2019-06-01 13:31:36 +0200

    first commit

---
+++ WarFly/GameScene.swift
@@ -0,0 +1,20 @@
+//
+//  GameScene.swift
+//  WarFly
+//
+//  Created by dmitrii on 01.06.19.
+//  Copyright © 2019 dmitrii. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        let sprite = SKSpriteNode(color: .blue, size: CGSize(width: 100, height: 100))
+        sprite.position = CGPoint(x: 200, y: 200)
+        self.addChild(sprite)
+    }
+
+}
---
+++ WarFly/GameViewController.swift
@@ -0,0 +1,46 @@
+//
+//  GameViewController.swift
+//  WarFly
+//
+//  Created by dmitrii on 01.06.19.
+//  Copyright © 2019 dmitrii. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+                // Set the scale mode to scale to fit the window
+            let scene = GameScene(size: self.view.bounds.size)
+            
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+
+    override var shouldAutorotate: Bool {
+        return true
+    }
+
+    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
+        return .portrait
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+}
